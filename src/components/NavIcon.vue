5174df704e2df58110b19ce867cb180802749653
Richard <rinaldy.richard.03@gmail.com>
2025-07-26 11:53:24 +0700

    feat: add nav icon

---
+++ src/components/NavIcon.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+defineProps({
+  icon: [String, Object],
+  alt: String,
+  name: String,
+})
+</script>
+
+<template>
+  <span>
+    <template v-if="typeof icon === 'string'">
+      <img :src="icon" :alt="alt || name" :class="'w-11 h-11' + ' object-cover'" />
+    </template>
+    <template v-else>
+      {{ name }}
+    </template>
+  </span>
+</template>
