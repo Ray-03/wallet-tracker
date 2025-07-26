5771fd766013f136b66abdc1890ee4bc8976806f
Richard <rinaldy.richard.03@gmail.com>
2025-07-26 17:18:04 +0700

    feat: separate nav

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,40 @@ import AppNav from './components/AppNav.vue'
   <header>
     <AppNav />
   </header>
-  <main>
+  <main class="p-4 pb-16 md:pb-0">
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Non corrupti perspiciatis
+    exercitationem in sapiente, minus laudantium sint pariatur saepe quisquam deserunt sunt aliquam
+    quas magnam amet rem nam, autem blanditiis deleniti accusantium ipsum similique! Corporis
+    ducimus commodi voluptatum, facilis at iure dolor ex reiciendis ea quos reprehenderit,
+    distinctio consectetur sunt corrupti soluta exercitationem! Ducimus facere voluptatum illum,
+    distinctio quidem dolor nisi veritatis dignissimos doloremque eos quasi tenetur tempora
+    recusandae, debitis laborum quod reiciendis exercitationem sit pariatur ut ratione, atque magnam
+    vero. Corporis excepturi doloremque laborum delectus ullam mollitia eum earum cum velit numquam
+    libero facilis est non eius enim exercitationem, tenetur obcaecati. Soluta perferendis
+    repudiandae neque, consectetur enim natus dolor id tempora at obcaecati earum non quidem
+    consequuntur ad expedita quisquam laboriosam laborum a quaerat tempore. Nobis nihil ipsum
+    possimus accusantium reiciendis assumenda alias sit aliquam, vero perferendis odio porro dolorem
+    commodi blanditiis, dignissimos deleniti quasi omnis eum natus eos consequuntur rem? Dolore,
+    dicta neque. Velit veritatis enim corporis amet! Iure error aliquid iusto soluta laudantium hic.
+    Asperiores sit illo provident modi assumenda placeat? Accusamus consequuntur officia, ab iusto
+    ad veniam temporibus modi obcaecati inventore animi rerum? Perspiciatis omnis maxime molestias
+    molestiae at. Quam repudiandae ipsa explicabo obcaecati modi illo. Lorem ipsum dolor sit amet
+    consectetur adipisicing elit. Corrupti, sint quis! Laborum dolore praesentium fuga excepturi at
+    deleniti rem odit tempore esse voluptate in quo laudantium perferendis quaerat nam, voluptas
+    iure consequuntur voluptatem qui odio! Voluptatem nemo ex repudiandae necessitatibus laudantium
+    tempora dignissimos eveniet, cumque facilis sunt expedita, ab sit distinctio dolore non velit
+    qui voluptatibus autem beatae consequuntur tenetur illo quia minus quod. Quos officia fuga sed
+    ab incidunt deleniti quidem autem dolorum dicta libero eius officiis id ratione voluptates ex
+    velit nostrum aliquam, optio soluta quisquam doloremque, ullam minima eum quam. Unde fuga harum
+    porro velit modi eveniet esse facere cumque dolores beatae! Quos voluptatem cumque fugiat
+    tenetur quod sint impedit voluptate atque nisi libero dolores dignissimos obcaecati odio enim,
+    odit soluta, corporis asperiores, eligendi iure. Iusto iure repellendus ducimus libero vitae
+    quos in dolorum at ut architecto nesciunt amet, sed tempora molestias necessitatibus reiciendis
+    animi magni unde praesentium provident corporis officia consectetur delectus veritatis.
+    Excepturi ex eum cupiditate voluptatem dolor a neque. Voluptatum debitis accusamus adipisci
+    cumque, possimus id iusto! Facere quibusdam repellendus commodi eveniet fugit vero quos! Quidem
+    ex distinctio earum deserunt atque, magni quae obcaecati? Doloremque dignissimos sequi id
+    laboriosam iste nemo at dicta impedit.
     <router-view />
   </main>
 </template>
--- src/components/AppNav.vue
+++ src/components/AppNav.vue
@@ -1,23 +1,40 @@
 <template>
-  <nav class="flex justify-between p-4 shadow md:flex md:flex-row flex-row-reverse">
-    <div class="flex flex-auto gap-4 flex-row-reverse items-center">
-      <a :href="cartLink.href" class="flex items-center justify-center">
-        <component :is="cartLink.icon" class="w-8 h-8 text-primary" />
-        <span class="sr-only">{{ cartLink.name }}</span>
-      </a>
+  <nav class="hidden md:flex justify-between items-center p-4 shadow flex-row">
+    <a
+      :href="homeLink.href"
+      class="flex items-center justify-center focus:outline-none focus:ring-2 focus:ring-blue-500"
+      aria-label="Go to Home"
+    >
+      <NavIcon :icon="homeLink.icon" :alt="homeLink.alt" :name="homeLink.name" />
+    </a>
+    <div class="flex-1 mx-4">
       <SearchInput class="w-full" />
-      <template v-for="link in navLinks" :key="link.name">
-        <a :href="link.href" class="hidden md:inline items-center justify-center">
+    </div>
+    <div class="flex gap-4 items-center">
+      <template v-for="link in desktopLinks" :key="link.name">
+        <a :href="link.href" class="items-center justify-center">
           <NavIcon :icon="link.icon" :alt="link.alt" :name="link.name" />
         </a>
       </template>
     </div>
   </nav>
-  <!-- Bottom Navbar (Mobile Only) -->
+  <nav class="flex md:hidden justify-between items-center p-4 shadow flex-row-reverse">
+    <a
+      v-for="link in mobileTopLinks"
+      :key="link.name + '-mobile-top'"
+      :href="link.href"
+      class="min-w-[44px] min-h-[44px] flex items-center justify-center"
+    >
+      <NavIcon :icon="link.icon" :alt="link.alt" :name="link.name" :img-size="'w-11'" />
+    </a>
+    <div class="flex-1 mx-2">
+      <SearchInput class="w-full" />
+    </div>
+  </nav>
   <nav
-    class="fixed bottom-0 left-0 right-0 shadow-inner flex flex-row-reverse justify-around p-2 md:hidden"
+    class="fixed bottom-0 left-0 right-0 shadow-inner flex justify-around p-2 md:hidden bg-white"
   >
-    <template v-for="link in navLinks" :key="link.name + '-mobile'">
+    <template v-for="link in mobileBottomLinks" :key="link.name + '-mobile-bottom'">
       <a :href="link.href" class="min-w-[44px] min-h-[44px] flex items-center justify-center">
         <NavIcon :icon="link.icon" :alt="link.alt" :name="link.name" :img-size="'w-11'" />
       </a>
@@ -29,13 +46,24 @@
 import NavIcon from './NavIcon.vue'
 import SearchInput from './SearchInput.vue'
 import IconShoppingBag from './icons/IconShoppingBag.vue'
+import IconMagnifier from './icons/IconMagnifier.vue'
+import { NavSection } from './types'
 import type { INavLink } from './types'
 
-const navLinks: INavLink[] = [
-  { name: 'History', href: 'history' },
-  { name: 'Wallet', href: 'wallet' },
-  { name: 'Home', href: '/', icon: '/icon.jpg', alt: 'Home' },
-]
+const baseLinks: Record<NavSection, Partial<INavLink> & { name: NavSection }> = {
+  [NavSection.Home]: { name: NavSection.Home, href: '/', icon: '/icon.jpg', alt: 'Home' },
+  [NavSection.Wallet]: { name: NavSection.Wallet, href: 'wallet' },
+  [NavSection.History]: { name: NavSection.History, href: 'history' },
+  [NavSection.Cart]: { name: NavSection.Cart, href: '#', icon: IconShoppingBag, alt: 'Cart' },
+  [NavSection.Search]: { name: NavSection.Search, href: '#', icon: IconMagnifier, alt: 'Search' },
+}
 
-const cartLink: INavLink = { name: 'Cart', href: '#', icon: IconShoppingBag, alt: 'Cart' }
+const desktopOrder = [NavSection.Wallet, NavSection.History, NavSection.Cart]
+const mobileTopOrder = [NavSection.Cart]
+const mobileBottomOrder = [NavSection.Wallet, NavSection.Home, NavSection.History]
+
+const desktopLinks = desktopOrder.map((name) => baseLinks[name])
+const mobileTopLinks = mobileTopOrder.map((name) => baseLinks[name])
+const mobileBottomLinks = mobileBottomOrder.map((name) => baseLinks[name])
+const homeLink = baseLinks[NavSection.Home]
 </script>
