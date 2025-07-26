d5023fa38c7349ebbcf695941fb9380eb1e5de6c
Richard <rinaldy.richard.03@gmail.com>
2025-07-26 09:00:16 +0700

    feat: use tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,35 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}', './public/**/*.html'],
+  theme: {
+    extend: {
+      colors: {
+        background: 'var(--color-background)',
+        'background-soft': 'var(--color-background-soft)',
+        'background-mute': 'var(--color-background-mute)',
+        border: 'var(--color-border)',
+        'border-hover': 'var(--color-border-hover)',
+        heading: 'var(--color-heading)',
+        text: 'var(--color-text)',
+        indigo: 'var(--vt-c-indigo)',
+      },
+      fontFamily: {
+        sans: [
+          'Inter',
+          '-apple-system',
+          'BlinkMacSystemFont',
+          'Segoe UI',
+          'Roboto',
+          'Oxygen',
+          'Ubuntu',
+          'Cantarell',
+          'Fira Sans',
+          'Droid Sans',
+          'Helvetica Neue',
+          'sans-serif',
+        ],
+      },
+    },
+  },
+  plugins: [],
+}
