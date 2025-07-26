6f80265b357a456e7f84c8805b3884e0ea4833c7
Richard <rinaldy.richard.03@gmail.com>
2025-07-26 11:40:44 +0700

    feat: add cancel search

--- src/components/SearchInput.vue
+++ src/components/SearchInput.vue
@@ -1,16 +1,32 @@
 <template>
-  <input
-    type="text"
-    placeholder="Search product..."
-    class="border rounded px-2 py-1 focus:outline-none focus:ring w-full"
-    v-model="searchQuery"
-    @input="$emit('update:modelValue', searchQuery)"
-    @keyup.enter="onEnter"
-  />
+  <div class="relative flex items-center w-full">
+    <span class="absolute left-2 text-gray-400">
+      <IconMagnifier />
+    </span>
+    <input
+      type="text"
+      placeholder="Search product..."
+      class="border rounded px-8 py-1 focus:outline-none focus:ring w-full"
+      v-model="searchQuery"
+      @input="$emit('update:modelValue', searchQuery)"
+      @keyup.enter="onEnter"
+      @keyup.esc="clearSearch"
+    />
+    <button
+      v-if="searchQuery"
+      @click="clearSearch"
+      class="absolute right-2 text-gray-400 hover:text-gray-600 focus:outline-none"
+      type="button"
+    >
+      <IconCancel />
+    </button>
+  </div>
 </template>
 
 <script setup lang="ts">
 import { ref, watch, defineProps, defineEmits } from 'vue'
+import IconCancel from './icons/IconCancel.vue'
+import IconMagnifier from './icons/IconMagnifier.vue'
 
 const props = defineProps<{ modelValue?: string }>()
 const emit = defineEmits(['update:modelValue', 'search'])
@@ -20,6 +36,11 @@ const searchQuery = ref(props.modelValue ?? '')
 function onEnter() {
   emit('search', searchQuery.value)
   console.log('Search triggered:', searchQuery.value)
+}
+
+function clearSearch() {
+  searchQuery.value = ''
+  emit('update:modelValue', '')
 }
 
 watch(
