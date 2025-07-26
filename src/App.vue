6ff5f929079981c1f828ef2021b05813aa6acf6b
Richard <rinaldy.richard.03@gmail.com>
2025-07-26 11:58:08 +0700

    feat: add appNav to app

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,14 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import AppNav from './components/AppNav.vue'
+</script>
 
 <template>
-  <header>asd</header>
-
-  <main></main>
+  <header>
+    <AppNav />
+  </header>
+  <main>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam ipsam eos sapiente veritatis
+    quos voluptatem, tenetur possimus aspernatur perspiciatis impedit ullam cum officia voluptates
+    voluptatibus, ut rerum, nisi saepe amet.
+  </main>
 </template>
