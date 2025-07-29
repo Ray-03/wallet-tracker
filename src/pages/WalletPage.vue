c9df42d969414fc083e6d329e8de5990bbce8e84
Richard <rinaldy.richard.03@gmail.com>
2025-07-29 16:36:10 +0700

    fix: remove unused container

--- src/pages/WalletPage.vue
+++ src/pages/WalletPage.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="min-h-screen bg-gray-50">
+  <div class="bg-gray-50">
     <div class="max-w-4xl mx-auto p-4 space-y-6">
       <WalletBalance @open-top-up="showTopUpModal = true" />
 
